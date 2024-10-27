a4bceb985ef6d44c7117123475b6f6a10216dd84
AKSHITARAJARORA <85892300+AKSHITARAJARORA@users.noreply.github.com>
2024-10-27 22:17:41 +0530

    Add files via upload

---
+++ Arrange An Array.js
@@ -0,0 +1,9 @@
+//Arrange an array
+//Ascending Order.
+var num=[12, 6, 8, 10, 11, 20]
+num.sort((a,b)=>(a-b))
+console.log("Ascending order- ",num)
+//Descending Order.
+var num=[12, 6, 8, 10, 11, 20]
+num.sort((a,b)=>(b-a))
+console.log("Descending order- ",num)
